1570d3f80ac51ff455828126ee7adf681f9201af
qas-source <q.ebsim@gmail.com>
2023-11-14 11:25:14 -0500

    Simple Screen

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,6 +1,25 @@
 package src;
 
-public class GameManager {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class GameManager extends Application {
 
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = new Pane();
+
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 }
