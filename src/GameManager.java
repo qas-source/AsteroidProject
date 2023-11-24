c1531160a7d13e817806ba29612b827ac59a6766
qas-source <q.ebsim@gmail.com>
2023-11-24 10:57:50 -0500

    Change main class

--- src/GameManager.java
+++ src/GameManager.java
@@ -6,20 +6,6 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class GameManager extends Application {
+public class GameManager {
 
-    public static void main(String[] args) {
-        launch();
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        Pane root = new Pane();
-
-        Scene scene = new Scene(root, 300, 200);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 package src;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    
+
     public static void main(String[] args) {
         launch(args);
 
@@ -18,5 +23,16 @@ public class Main extends Application {
         GraphicsContext draw = canvas.getGraphicsContext2D();
         draw.setFill(Color.BLACK);
 
+        // create a Group
+        Group group = new Group(canvas);
+
+
+        // create a scene
+        Scene scene = new Scene(group, 200, 200);
+
+        // set the scene
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
